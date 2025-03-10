c9bcde81ed23136a31862e0f7def77b8ce13df8d
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-03-10 16:39:03 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <h1 className="text-xl font-light tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
-        <span className="block">To Do List</span>
+        <span className="block">Auto Delete Todo List</span>
       </h1>
       <SeparatorLine />
       <ToDoList />
